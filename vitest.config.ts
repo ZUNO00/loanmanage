f13d5832d5e98963729f0d697e026a45413aa973
ZUNO00 <81406094+ZUNO00@users.noreply.github.com>
2026-09-07 11:32:09 +0700

    fix: remove unnecessary type:module and revert vitest.config.ts to brief spec

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import path from 'node:path'
 
-const __dirname = import.meta.dirname
-
 export default defineConfig({
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
